5ea238cc7bb479be559dad31974eb2b8ee90b331
shivam779823 <105196334+shivam779823@users.noreply.github.com>
2022-07-04 00:58:04 -0400

    modified main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable "location" {
 }
 
 variable "network_name" {
-  default = "tf-gke-k8s"
+  default = "cluster_net"
 }
 
 provider "google" {
@@ -35,20 +35,18 @@ data "google_container_engine_versions" "default" {
 }
 
 resource "google_container_cluster" "default" {
-  name               = var.network_name
+  name               = "sample_cluster"
   location           = var.location
   initial_node_count = 3
   min_master_version = data.google_container_engine_versions.default.latest_master_version
   network            = google_compute_subnetwork.default.name
   subnetwork         = google_compute_subnetwork.default.name
 
-  // Use legacy ABAC until these issues are resolved: 
-  //   https://github.com/mcuadros/terraform-provider-helm/issues/56
-  //   https://github.com/terraform-providers/terraform-provider-kubernetes/pull/73
-  enable_legacy_abac = true
+  
+  
+    enable_legacy_abac = true
 
-  // Wait for the GCE LB controller to cleanup the resources.
-  // Wait for the GCE LB controller to cleanup the resources.
+    
   provisioner "local-exec" {
     when    = destroy
     command = "sleep 90"
